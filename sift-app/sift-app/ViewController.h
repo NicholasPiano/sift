afb0a0783275d701598bab4802012eb87209c360
NicholasPiano <nicholas.d.piano@gmail.com>
2015-02-09 10:35:46 +0000

    Initial

---
+++ sift-app/sift-app/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sift-app
+//
+//  Created by Nicholas Piano on 09/02/2015.
+//  Copyright (c) 2015 Nicholas Piano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ sift-app/sift-app/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  sift-app
+//
+//  Created by Nicholas Piano on 09/02/2015.
+//  Copyright (c) 2015 Nicholas Piano. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
